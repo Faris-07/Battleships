2de45d62f2feeb8d2c19ad85d33a6e544b71a856
Faris-07 <farisuddin03@gmail.com>
2022-02-12 17:07:28 +0000

    Create player_ship_loc function to allow the user to place their battleships

--- run.py
+++ run.py
@@ -39,3 +39,15 @@ def create_ships(board):
         board[ship_row][ship_column] = "X"
 
 def player_ship_loc():
+    """
+    Player chooses where to place their 5 ships
+    """
+    row = input("Enter the row of the ship: ").upper()
+    while row not in "12345678":
+        print('Not an appropriate choice, please select a valid row')
+        row = input("Enter the row of the ship: ").upper()
+    column = input("Enter the column of the ship: ").upper()
+    while column not in "ABCDEFGH":
+        print('Not an appropriate choice, please select a valid column')
+        column = input("Enter the column of the ship: ").upper()
+    return int(row) - 1, letters_conversion[column]
